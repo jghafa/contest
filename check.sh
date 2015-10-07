6efb67e41732ceb0ede9ff05aaedebf6c256fe01
jghafa <james.haley@yahoo.com>
2015-10-06 22:32:47 -0400

    matches answer to solution by file name

--- check.sh
+++ check.sh
@@ -10,19 +10,21 @@ shopt -s nullglob
 
 
 # Make sure answers exist
-if [ -d "answers" ] ; then
-#  echo answers exist
+if [ -d "answers" ]; 
+then
+  echo 
   answers=(./answers/[0-9][0-9]*.txt)
-  echo number of answer files ${#answers[@]} 
-  if [ ${#answers[@]} -eq 0 ]; then
-    echo No answers were detected in the answers directory
+  echo ${#answers[@]} 'Answers files loaded'
+  if [ ${#answers[@]} -eq 0 ]; 
+  then
+    echo 'No answers were detected in the answers directory'
     exit
   fi  
 else
-echo if false
+  echo 'answers exist' 
   mkdir answers
-  echo Directory 'answers' created, please fill with answer files.
-  echo File name format: 00.txt for the answer to question 0
+  echo 'Directory answers created, please fill with answer files.'
+  echo 'File name format: 00.txt for the answer to question 0'
   exit
 fi
 
@@ -32,22 +34,25 @@ fi
 #echo $1*.txt
 
 # Read the files names from the specified directory
-array=($1*.txt)
+array=($1[0-9][0-9]*.txt)
 echo
-echo number of files found ${#array[@]} 
-echo files found
-echo "${array[@]}" # echo elements
-echo 
+echo ${#array[@]} 'Files submitted for review' 
+#echo 'files found'
+#echo "${array[@]}" # echo file names
+#echo 
 
 #Check each file
 for i in "${array[@]}"
 do
-# next feature to add - get first char from file name, use to get answer file
-  diff $i ./answers/00.txt > /dev/null
+# get first chars from file name, use to get answer file
+  echo 
+  j="${i:${#1}:2}"   # Extract problem number, skip param 1 length, then get 2
+echo $j
+  diff -w $i ./answers/${j}.txt > /dev/null
   if [ $? -ne 0 ]; then    # did it match
-    echo $i did not match          # nope
+    echo $i 'did not match'          # nope
   else
-    echo $i Matched
+    echo $i 'Matched'
   fi
 done
 #echo direct ${array[0]}
